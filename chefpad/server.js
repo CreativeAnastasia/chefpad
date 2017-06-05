faba8f1eba40c8055fd1278b93d2aca0ff3a6760
Varanetskaya <creativeanastasia@gmail.com>
2017-06-05 16:38:02 -0700

    testing

--- chefpad/server.js
+++ chefpad/server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require ('body-parser');
+var favicon = require('serve-favicon');
 
 require('dotenv').config();
 require('./config/database');
